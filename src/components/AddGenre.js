648a5a614de8874cadd46f6d88cd1876ee0bb234
Katie <katiefaybehrmann@gmail.com>
2023-03-30 17:25:31 -0600

    Build out create form

--- src/components/AddGenre.js
+++ src/components/AddGenre.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddGenre(){
-    <div>
-        <p>hold</p>
-    </div>
+function AddGenre({onAddGenre}){
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+
+    return(
+        <form>
+            <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            />
+            <input 
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+        </form>
+    )
+
 }
 
 export default AddGenre
--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,7 @@ function App() {
       <p>Yes, this is my personal record collection</p>
       <p>Not affiliated with Discogs...</p>
       <GenreList genres={genres}/>
+      <AddGenre onAddGenre={handleAddGenre}/>
     </div>
   );
 }
